4c3257a220fcc787340e10fa5fb177df215f6ebe
ethanchen1301 <84855099+ethanchen1301@users.noreply.github.com>
2023-08-23 16:40:30 -0400

    Add files via upload

--- SSDraw/examples/run_examples.sh
+++ SSDraw/examples/run_examples.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 
 # example 1
-python3 ../SSDraw.py --fasta 1ndd.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_out
+python3 ../SSDraw.py --fasta 1ndd.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_out --dpi 1000
 
 # example 2: score by conservation
-python3 ../SSDraw.py --fasta aligned.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_conservation -conservation_score
+python3 ../SSDraw.py --fasta aligned.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_conservation -conservation_score --start 80 --end 162 --dpi 1000
 
 # example 3: score by bfactor
-python3 ../SSDraw.py --fasta 1ndd.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_bfactor -bfactor
+python3 ../SSDraw.py --fasta 1ndd.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_bfactor -bfactor --dpi 1000
 
 # example 4: Custom scoring file with custom color map
-python3 ../SSDraw.py --fasta 2kdl.fasta --name 2kdl --pdb 2kdl.pdb --output 2kdl_out --scoring_file 2kdl_scoring.txt --color_map black cyan  
-
-#Example 5: Choose subregion of alignment to run SSDraw on
-python3 ../SSDraw.py --fasta aligned.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd_conservation_cropped -conservation_score --start 80 --end 132
+python3 ../SSDraw.py --fasta 2kdl.fasta --name 2kdl --pdb 2kdl.pdb --output 2kdl_out --scoring_file 2kdl_scoring.txt --color_map black cyan --dpi 1000
---
+++ SSDraw/figures/draw_all_figures.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd GA-GB
+./run_ssdraw.sh
+cd ../
+
+cd ubiquitin
+./run_ssdraw.sh
+cd ../
+
+cd workflow
+./run_ssdraw.sh
+cd ../
