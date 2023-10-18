f8a5df7fed6179c925e39f6738594147ece31006
porterll <75682642+porterll@users.noreply.github.com>
2023-10-18 17:39:16 -0400

    Add files via upload

--- SSDraw/examples/run_examples.sh
+++ SSDraw/examples/run_examples.sh
@@ -11,3 +11,9 @@ python3 ../SSDraw.py --fasta 1ndd.fasta --name 1ndd --pdb 1ndd.pdb --output 1ndd
 
 # example 4: Custom scoring file with custom color map
 python3 ../SSDraw.py --fasta 2kdl.fasta --name 2kdl --pdb 2kdl.pdb --output 2kdl_out --scoring_file 2kdl_scoring.txt --color_map black cyan --dpi 1000
+
+#example 5: automatically generate stacked diagrams
+python3 ../run_multiple_pdbs_on_one_msa.py -i example_run.txt -o ubiquitin_stacked
+
+#example 6: color by Rate4site scores and add ticks
+python3 ../SSDraw.py -p 2n1v.pdb -f 2n1v.fasta -n 2n1v -o 2n1v_consurf --consurf 2n1v_nscores.txt --ticks 5
